369fb4de9ab42a270baa321e8f050327621306fe
Lecerof <j.lecerof@gmail.com>
2018-06-04 21:35:59 +0200

    pathchanges

--- Digital loggbok/Loggbok2.0/src/paths.py
+++ Digital loggbok/Loggbok2.0/src/paths.py
@@ -10,10 +10,14 @@ src_path = project_path + "/src/"
 res_path = project_path + "/res/"
 reg_path = project_path + "/reg/"
 stat_path = project_path + "/stat/"
-log_online_path = project_path + '/log/'
+#log_online_path = project_path + '/log/'
+# Absolute paths for sharing
+log_online_path = '/home/eXPerimentverkstaden/info/'
+mnt_loggbook_path = '/mnt/DigitalLoggbok/'
+
 # Absolut mapp
 #webpage_resources_path = '/mnt/www/incheckade/'
-webpage_resources_path = ''
+webpage_resources_path = '/home/eXPerimentverkstaden/incheckade/'
 
 # filnamn
 file_name_bg = 'bg.gif'
@@ -23,7 +27,7 @@ file_name_new_member_register = 'Nyamedlemmar.xlsx'
 # hela sökvägen
 gui_bg = res_path + file_name_bg
 xlsx_logg_online = log_online_path + file_name_logg_online
-xlsx_member_register = reg_path + file_name_member_register
-xlsx_new_members = reg_path + file_name_new_member_register
+xlsx_member_register = mnt_loggbook_path + file_name_member_register
+xlsx_new_members = mnt_loggbook_path + file_name_new_member_register
 
 xlsx_statistics = stat_path
